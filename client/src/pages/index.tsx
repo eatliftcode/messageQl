b8ed85005cb661fe24fc725eda8b8823334bd9e2
Mohit-vori <105245641+eatliftcode@users.noreply.github.com>
2022-10-18 11:22:40 -0500

    implemented next auth sign in and sign out

--- client/src/pages/index.tsx
+++ client/src/pages/index.tsx
@@ -1,9 +1,21 @@
 import type { NextPage } from 'next'
-
+import {signIn,signOut, useSession} from "next-auth/react";
 
 const Home: NextPage = () => {
+  const {data} = useSession();
+
+  console.log("HERE is data", data)
   return (
-      <h1>Hello world!</h1>
+    <div>
+      {
+        data?.user ?      
+        <button onClick={() => signOut()}>Sign Out</button>
+       :
+        <button onClick={() => signIn('google')}>Sign In</button>
+      }
+
+      
+    </div>
   )
 }
 
