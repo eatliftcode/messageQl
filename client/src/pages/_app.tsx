5e8e00ff09cdfeded6971da653d58a7bf86809f5
Mohit Gupta <mohit.gpta28@gmail.com>
2022-10-18 18:35:38 -0500

    apollo client setup for FE

--- client/src/pages/_app.tsx
+++ client/src/pages/_app.tsx
@@ -1,13 +1,19 @@
 import { SessionProvider } from 'next-auth/react'
 import { ChakraProvider } from '@chakra-ui/react'
 import {theme} from "../chakra/theme"
+import { ApolloProvider } from '@apollo/client'
+import { client } from '../graphql/apollo-client'
+
+
 function MyApp({ Component, pageProps } : any) {
   return (
+    <ApolloProvider client={client}>
     <SessionProvider session={pageProps.session}>
       <ChakraProvider theme = {theme}>
           <Component {...pageProps} />
       </ChakraProvider>
     </SessionProvider>
+    </ApolloProvider>
   )
 }
 
